57d0dce995d543bb9b9137e0d28f63dfa169d12f
Edson Rodrigues <edson.er.rodrigues@gmail.com>
2022-06-16 17:41:07 -0300

    Criei uns arquivos novos

---
+++ Itens.py
@@ -0,0 +1,9 @@
+class Item:
+
+    def __init__(self, nome, isMeele, peso, raridade, bonusAtributo):
+        self.nome = nome
+        self.isMeele = isMeele
+        self.peso = peso
+        self.raridade = raridade
+        self.bonusAtributo = bonusAtributo
+        
